03b7371d1b02a88304a6a389acefa8e727f6330d
Surjanthakur <tsurjan506@gmail.com>
2025-11-24 12:49:47 +0530

    save

--- auth/db.py
+++ auth/db.py
@@ -9,11 +9,13 @@ database_url = os.getenv("DB_URL")
 engine = create_engine(database_url)
 
 
+# create the database and tables
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
     print("Database and tables created.👍")
 
 
+# Dependency to get DB session
 def get_session():
     with Session(engine) as session:
         yield session
