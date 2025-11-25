f016ae99485fbb257fed32c219adb8fe189336dd
Surjanthakur <tsurjan506@gmail.com>
2025-11-25 22:30:05 +0530

    save

--- auth/router/notes.py
+++ auth/router/notes.py
@@ -30,7 +30,9 @@ def get_all_notes(session_db: Session = Depends(get_session)):
 def create_new_note(notes_data: InputNotes, session_db: Session = Depends(get_session)):
     try:
         new_notes = Notes(
-            title=notes_data.title, content=notes_data.content, owner=notes_data.owner
+            title=notes_data.title,
+            content=notes_data.content,
+            owner=notes_data.owner.strip(),
         )
         session_db.add(new_notes)
         session_db.commit()
@@ -40,4 +42,47 @@ def create_new_note(notes_data: InputNotes, session_db: Session = Depends(get_se
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An error occurred while creating a new note in db 😳: {str(err)}",
+        )
+
+
+@router.put("/update/{email_id}")
+def update_note(
+    email_id: str,
+    update_data: ShowNotes,
+    session_db: Session = Depends(get_session),
+):
+    update_note = session_db.exec(select(Notes).where(Notes.owner == email_id)).first()
+    if not update_note:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Note not found 😳"
+        )
+    try:
+        update_note.title = update_data.title
+        update_note.content = update_data.content
+        session_db.add(update_note)
+        session_db.commit()
+        session_db.refresh(update_note)
+        return {"detail": "Note updated successfully 😊"}
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred while updating the note in db 😳: {str(err)}",
+        )
+
+
+@router.delete("/delete/{id}")
+def delete_note(id: str, session_db: Session = Depends(get_session)):
+    delete_note = session_db.exec(select(Notes).where(Notes.id == id)).first()
+    if not delete_note:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Note not found 😳"
+        )
+    try:
+        session_db.delete(delete_note)
+        session_db.commit()
+        return {"detail": "Note deleted successfully 😊"}
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred while deleting the note from db 😳: {str(err)}",
         )
