716e6afdd49fc30fe8f0ff1a0ef00f7347d340ff
Surjanthakur <tsurjan506@gmail.com>
2026-01-09 11:03:30 +0530

    save

--- crud-api/main.py
+++ crud-api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from data_validation import Patient_data
 import json
 
 app = FastAPI()
@@ -12,6 +13,15 @@ def load_data():
 
 
 @app.get("/all-patient")
-def get_allPatients():
+def get_all_patients():
     data = load_data()
     return {f"all-patients: {data}"}
+
+
+@app.get("/patient/{id}")
+def get_patient_by_id(id: str):
+    response = load_data()
+    if id in response:
+        return response[id]
+    else:
+        return {"cant find patient with this id!!"}
