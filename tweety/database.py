6340684ff8ee67ab6e52bb2597ae15f8538d172b
Surjanthakur <tsurjan506@gmail.com>
2026-01-16 22:51:44 +0530

    save

---
+++ tweety/database.py
@@ -0,0 +1,23 @@
+# This file handles database setup, connection and session management for the Tweety app.
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Session, SQLModel, create_engine, select
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+
+engine = create_engine(DB_URL)
+
+
+# create the database and tables
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+# get the session for the database on per request.
+def get_session():
+    with Session(engine) as session:
+        yield session
--- tweety/main.py
+++ tweety/main.py
@@ -1,6 +1,3 @@
-def main():
-    print("Hello from tweety!")
+from fastapi import FastAPI
 
-
-if __name__ == "__main__":
-    main()
+app = FastAPI()
