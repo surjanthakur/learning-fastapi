8c2e745f07123ec55ce73aab41901aff12f21e4a
Surjanthakur <tsurjan506@gmail.com>
2026-01-17 13:22:50 +0530

    save

--- tweety/sql_model.py
+++ tweety/sql_model.py
@@ -1,18 +1,28 @@
 from pydantic import EmailStr
-import datetime
-from sqlmodel import Field, SQLModel
+from datetime import datetime
+from sqlmodel import Field, Relationship, SQLModel
+from typing import List, Optional
+import uuid
 
 
 class User(SQLModel, table=True):
-    id: str = Field(primary_key=True, default=None)
+    id: str = Field(default_factory=lambda: str(uuid.uuid5()), primary_key=True)
     name: str = Field(min_length=3, max_length=20)
     email: EmailStr = Field(unique=True)
-    created_at: str = Field(default=datetime.date.today())
-    updated_at: str = Field(default=datetime.date.today())
+    tweets: List["Tweet"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
+    created_at: datetime = Field(default_factory=datetime.now())
+    updated_at: datetime = Field(default_factory=datetime.now())
 
 
 class Tweet(SQLModel, table=True):
-    id: str = Field(primary_key=True, default=None)
+    id: str = Field(default_factory=lambda: str(uuid.uuid5()), primary_key=True)
     content: str = Field(min_length=10, max_length=400)
-    created_at: str = Field(default=datetime.date.today())
-    updated_at: str = Field(default=datetime.date.today())
+    user_id: str = Field(
+        ...,
+        nullable=False,
+        foreign_key="user.id",
+        foreign_key_kwargs={"ondelete": "CASCADE"},
+    )
+    user: Optional["User"] = Relationship(back_populates="tweets")
