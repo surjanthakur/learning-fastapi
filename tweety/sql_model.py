5fe75cd10dd428ec62109824d9458804bc12d67e
Surjanthakur <tsurjan506@gmail.com>
2026-01-17 17:40:18 +0530

    save

--- tweety/sql_model.py
+++ tweety/sql_model.py
@@ -6,23 +6,18 @@ import uuid
 
 
 class User(SQLModel, table=True):
-    id: str = Field(default_factory=lambda: str(uuid.uuid5()), primary_key=True)
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     name: str = Field(min_length=3, max_length=20)
     email: EmailStr = Field(unique=True)
     tweets: List["Tweet"] = Relationship(
         back_populates="user", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
     )
-    created_at: datetime = Field(default_factory=datetime.now())
-    updated_at: datetime = Field(default_factory=datetime.now())
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default=None)
 
 
 class Tweet(SQLModel, table=True):
-    id: str = Field(default_factory=lambda: str(uuid.uuid5()), primary_key=True)
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     content: str = Field(min_length=10, max_length=400)
-    user_id: str = Field(
-        ...,
-        nullable=False,
-        foreign_key="user.id",
-        foreign_key_kwargs={"ondelete": "CASCADE"},
-    )
+    user_id: str = Field(..., nullable=False, foreign_key="user.id", ondelete="CASCADE")
     user: Optional["User"] = Relationship(back_populates="tweets")
